9a17fe5e019a19d9672c02d309e9b524410ef26e
vishenkahimik <anastasyabormotova@gmail.com>
2022-07-31 19:04:25 +0300

    feat: add desicion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let OPEN_BRACKETS = [];
+  let PAIRS_BRACKETS = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let key = bracketsConfig[i][1];
+    let value = bracketsConfig[i][0];
+    OPEN_BRACKETS.push(bracketsConfig[i][0]);
+    PAIRS_BRACKETS[key] = value;
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+    if (currentSymbol === topElement && currentSymbol == PAIRS_BRACKETS[currentSymbol]) {
+      stack.pop();
+    } else if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      if (PAIRS_BRACKETS[currentSymbol] === topElement) {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length === 0;
+};
